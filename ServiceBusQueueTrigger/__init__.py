8f1e45ffa40295391c684f9d87e972399c1d8f0c
Chan Rui Jia <ruijia.chan@gmail.com>
2023-06-29 14:01:02 +0700

    Minor changes

--- ServiceBusQueueTrigger/__init__.py
+++ ServiceBusQueueTrigger/__init__.py
@@ -2,23 +2,29 @@ import os
 import json
 import logging
 import azure.functions as func
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
 from linebot import LineBotApi
 from linebot.exceptions import LineBotApiError
 from message_templates import *
 
-CHANNEL_ACCESS_TOKEN = os.environ.get("channel_access_token")
-line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+KEY_VAULT_NAME = os.environ.get("KEY_VAULT_NAME")
+# KEY_VAULT_NAME = "aira-dev-kv"  # set as environ when deploying
 
 
 def main(msg: func.ServiceBusMessage):
     formatted_msg = format_message(msg)
     logging.info(formatted_msg)
+    channel_access_token = get_channel_access_token(f"token{formatted_msg['channelid']}")
+    line_bot_api = LineBotApi(channel_access_token)
     try:
         line_bot_api.reply_message(formatted_msg["replyToken"], format_text(formatted_msg["text"]))
     except LineBotApiError:
-        logging.info("LineBotApiError")
+        logging.info("Reply token expired")
         line_bot_api.push_message(formatted_msg['userId'], format_text(formatted_msg["text"]))
     return
+
+    # send different messages based on type
 
     # user_id = formatted_msg["user_id"]
     # message_type = formatted_msg["message_type"]
@@ -41,6 +47,9 @@ def format_message(msg):
     msg_str = msg.get_body().decode('utf-8')
     msg_json = json.loads(msg_str)
     return msg_json  # testing
+
+    # format json to pass into message templates
+
     # formatted_json = {
     #     "user_id": msg_json.get("userId"),
     #     "message_type": msg_json.get("message", {}).get("type"),
@@ -63,3 +72,10 @@ def format_text(text):
     logging.info(formatted_messages)
 
     return formatted_messages
+
+
+def get_channel_access_token(channel_id):
+    KVUri = f"https://{KEY_VAULT_NAME}.vault.azure.net"
+    credential = DefaultAzureCredential()
+    client = SecretClient(vault_url=KVUri, credential=credential)
+    return client.get_secret(channel_id).value
